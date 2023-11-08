9a31f1ac53833dce23780c2ba236f6e434581adf
Rakibul Islam <rakibul4516>
2023-11-08 07:32:58 +0600

    this commit adds fetch all foods using axios and implement the search functionality

---
+++ src/Components/FoodCards.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { FcGlobe, } from "react-icons/fc";
+const FoodCards = ({ data }) => {
+    const { _id, foodName, image, category, quantity, price, origin } = data || {}
+    return (
+        <div className="w-full overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
+            <img className="object-cover object-center w-full h-56" src={image} alt="avatar" />
+
+            <div className="px-3 py-2 bg-orange-100">
+                <h1 className="text-xl font-semibold font-[Suranna] text-gray-800 dark:text-white">{foodName}</h1>
+
+            </div>
+
+            <div className="flex items-center justify-between px-2 py-2">
+                <div className="flex items-center">
+                    <FcGlobe className=' text-lg font-semibold'></FcGlobe>
+                    <h1 className="mx-2 text-lg font-semibold text-slate-700">{origin}</h1>
+                </div>
+                <div className="flex">
+                    <h1 className="mx-3 text-xl font-semibold text-slate-700">$ {price}</h1>
+                </div>
+            </div>
+            <div className="px-2 pb-4 flex gap-3 justify-between items-center">
+                <h1 className="text-lg font-semibold text-gray-800 dark:text-white">{category}</h1>
+                <h2 className="text-lg font-semibold text-gray-800 dark:text-white">Qty: {quantity}</h2>
+                <Link to={`/details/${_id}`} className="px-3 py-2 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-orange-400 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Details</Link>
+            </div>
+
+        </div>
+    );
+};
+
+FoodCards.propTypes = {
+    data: PropTypes.object,
+};
+
+export default FoodCards;
